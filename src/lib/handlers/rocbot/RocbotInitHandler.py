7c6877ee242198b56dfb4e1d0a936254d5d185ca
Adrian <adiboteanu@outlook.com>
2015-12-17 15:06:18 -0500

    updated config files to call rocbot handlers

--- src/lib/handlers/rocbot/RocbotInitHandler.py
+++ src/lib/handlers/rocbot/RocbotInitHandler.py
@@ -2,9 +2,9 @@
 Rocbot handler, the simulator is assumed to be already running
 """
 
-import lib.handlers.handlerTemplates as handlerTemplates
-from rocbot import *
 import lcm 
+from rocbot import *
+import lib.handlers.handlerTemplates as handlerTemplates
 
 class RocbotInitHandler(handlerTemplates.InitHandler):
 	def __init__(self, executor):
@@ -12,18 +12,11 @@ class RocbotInitHandler(handlerTemplates.InitHandler):
 		self.lc = lcm.LCM()
         	# subscribe to world state channel
 		self.subscription = self.lc.subscribe( "STATE_MODEL_ROCBOT", self.world_state_handler )
-
-		self.state_message = None
-
-		try:
-			while True:
-				self.lc.handle()
-		except KeyboardInterrupt:
-			pass
+		self.state_message = dict()
 
 	def world_state_handler( self, channel, data ):
-		self.state_message = state_model_msg_t.decode(data)
-		#print self.state_message.id
+		msg = state_model_msg_t.decode(data)
+		self.state_message[msg.id] = msg
 
 	def getSharedData(self):
 		return { "ROCBOT_INIT_HANDLER" : self }
--- src/lib/handlers/rocbot/RocbotSensorHandler.py
+++ src/lib/handlers/rocbot/RocbotSensorHandler.py
@@ -8,6 +8,25 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 	def __init__(self, executor, shared_data):
 		self.RocbotInitHandler = shared_data['ROCBOT_INIT_HANDLER']
 
+		self.object_types = {
+		"OBJECT_TYPE_CUBE" : "cube",
+		"OBJECT_TYPE_HOLDER" : "holder",
+		"OBJECT_TYPE_BIN" : "bin",
+		"OBJECT_TYPE_TRAY" : "tray",
+		"OBJECT_TYPE_TABLE" : "table",
+		"OBJECT_TYPE_ROBOT_TORSO" : "robot_torso",
+		"OBJECT_TYPE_ROBOT_LEFT_HAND" : "robot_left_hand",
+		"OBJECT_TYPE_ROBOT_RIGHT_HAND" : "robot_right_hand",
+		"OBJECT_TYPE_U_BLOCK" : "u_block"
+		}
+
+		self.object_color = {
+		"OBJECT_COLOR_RED" : "red",
+		"OBJECT_COLOR_BLUE" : "blue",
+		"OBJECT_COLOR_GREEN" : "green",
+		"na", "na"
+		}
+
 # state message format
 #  int64_t timestamp;
 #  string id;
@@ -16,41 +35,147 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 #  int32_t num_state_bodies;
 #  state_body_msg_t state_bodies[ num_state_bodies ];
 
-	def sensor_handler(self, sensor_type, object_list, initial=False):
-		'''
-		types defined in ltl_h2sl symbols
-		sensor_type - string
-		object_list - list of dict {'obj_type':'TYPE', 'obj_color':'COLOR'}
-		'''
+
+	def listen_for_state(self):
+		try:
+			for i in range(10): # this should be a the number of objects in the world
+				self.RocbotInitHandler.lc.handle()
+                except KeyboardInterrupt:
+                        pass
+
+	# TODO
+	def sensor_type_observed(self, object_type, object_color, initial=False):
+		"""
+		object_type (string): must be in self.object_types
+		object_color (string): must be in self.object_colors
+		"""
 		if initial:
 			return False
 		try:
-			if not self.RocbotInitHandler.state_message:
-				return False
+			print '#', self.RocbotInitHandler.keys()
+		except:
+			pass
+			
+		return False
 
-			if sensor_type == "SENSOR_TYPE_OBSERVED":
-			# look for a single object in the scene
-				assert (len( object_list ) > 0)
-				return self.object_handler( object_list[0]['obj_type'], object_list[0]['obj_color'] )
-				
-			elif sensor_type == "SENSOR_TYPE_COVERED":
-				return False
-			elif sensor_type == "SENSOR_TYPE_CLEAR":
-				return False
-			elif sensor_type == "SENSOR_TYPE_FULL":
-				return False
-			elif sensor_type == "SENSOR_TYPE_LEFT":
-				return False
-			elif sensor_type == "SENSOR_TYPE_RIGHT":
-				return False
-			elif sensor_type == "SENSOR_TYPE_FRONT":
-				return False
-			elif sensor_type == "SENSOR_TYPE_BACK":
-				return False
-			else:
-				print 'Unknown sensor type', sensor_type
-				pass
-		except Exception, e:
-			print "Error msg %s" % (str(e))
+	# TODO
+	def sensor_type_covered(self, object_type, object_color, initial=False):
+		"""
+		object_type (string): must be in self.object_types
+		object_color (string): must be in self.object_colors
+		"""
+		if initial:
+			return False
+		try:
+			print self.RocbotInitHandler.keys()
+		except:
+			pass
+			
+		return False
+
+	# TODO
+	def sensor_type_clear(self, object_type, object_color, initial=False):
+		"""
+		object_type (string): must be in self.object_types
+		object_color (string): must be in self.object_colors
+		"""
+		if initial:
+			return False
+		try:
+			print self.RocbotInitHandler.keys()
+		except:
+			pass
+			
+		return False
+
+	# TODO
+	def sensor_type_full(self, object_type, object_color, initial=False):
+		"""
+		object_type (string): must be in self.object_types
+		object_color (string): must be in self.object_colors
+		"""
+		if initial:
+			return False
+		try:
+			print self.RocbotInitHandler.keys()
+		except:
+			pass
+			
+		return False
+
+	# TODO
+	def sensor_type_left(self, object_type1, object_color1, object_type2, object_color2, initial=False):
+		"""
+		object 1 is relative to object 2 
+
+		object_type1 (string): must be in self.object_types
+		object_color1 (string): must be in self.object_colors
+		object_type2 (string): must be in self.object_types
+		object_color2 (string): must be in self.object_colors
+		"""
+		if initial:
+			return False
+		try:
+			print self.RocbotInitHandler.keys()
+		except:
+			pass
+			
+		return False
+
+	# TODO
+	def sensor_type_right(self, object_type1, object_color1, object_type2, object_color2, initial=False):
+		"""
+		object 1 is relative to object 2 
+
+		object_type1 (string): must be in self.object_types
+		object_color1 (string): must be in self.object_colors
+		object_type2 (string): must be in self.object_types
+		object_color2 (string): must be in self.object_colors
+		"""
+		if initial:
+			return False
+		try:
+			print self.RocbotInitHandler.keys()
+		except:
+			pass
+			
+		return False
+
+	# TODO
+	def sensor_type_front(self, object_type1, object_color1, object_type2, object_color2, initial=False):
+		"""
+		object 1 is relative to object 2 
+
+		object_type1 (string): must be in self.object_types
+		object_color1 (string): must be in self.object_colors
+		object_type2 (string): must be in self.object_types
+		object_color2 (string): must be in self.object_colors
+		"""
+		if initial:
+			return False
+		try:
+			print self.RocbotInitHandler.keys()
+		except:
+			pass
+			
+		return False
+
+	# TODO
+	def sensor_type_back(self, object_type1, object_color1, object_type2, object_color2, initial=False):
+		"""
+		object 1 is relative to object 2 
+
+		object_type1 (string): must be in self.object_types
+		object_color1 (string): must be in self.object_colors
+		object_type2 (string): must be in self.object_types
+		object_color2 (string): must be in self.object_colors
+		"""
+		if initial:
+			return False
+		try:
+			print self.RocbotInitHandler.keys()
+		except:
+			pass
+			
 		return False
 
