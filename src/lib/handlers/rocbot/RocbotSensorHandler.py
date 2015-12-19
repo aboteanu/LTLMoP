08872233056ccfa535fe502134850809e414354d
Adrian <adiboteanu@outlook.com>
2015-12-19 17:43:53 -0500

    match objects

--- src/lib/handlers/rocbot/RocbotSensorHandler.py
+++ src/lib/handlers/rocbot/RocbotSensorHandler.py
@@ -2,28 +2,30 @@ import threading, subprocess, os, time, socket
 import numpy, math
 import sys
 
+import lcm
+from rocbot import state_model_msg_t
+
 import lib.handlers.handlerTemplates as handlerTemplates
 
-import lcm
-
+state_msg = None
 
 class RocbotSensorHandler(handlerTemplates.SensorHandler):
 	def __init__(self, executor, shared_data):
 		self.RocbotInitHandler = shared_data['ROCBOT_INIT_HANDLER']
 
-		self.object_types = {
-		"OBJECT_TYPE_CUBE" : "cube",
-		"OBJECT_TYPE_HOLDER" : "holder",
-		"OBJECT_TYPE_BIN" : "bin",
-		"OBJECT_TYPE_TRAY" : "tray",
-		"OBJECT_TYPE_TABLE" : "table",
-		"OBJECT_TYPE_ROBOT_TORSO" : "robot_torso",
-		"OBJECT_TYPE_ROBOT_LEFT_HAND" : "robot_left_hand",
-		"OBJECT_TYPE_ROBOT_RIGHT_HAND" : "robot_right_hand",
-		"OBJECT_TYPE_U_BLOCK" : "u_block"
+		object_types = {
+		"OBJECT_TYPE_CUBE" : ["cube"],
+		"OBJECT_TYPE_HOLDER" : ["holder"],
+		"OBJECT_TYPE_BIN" : ["bin"],
+		"OBJECT_TYPE_TRAY" : ["tray"],
+		"OBJECT_TYPE_TABLE" : ["table"],
+		"OBJECT_TYPE_ROBOT_TORSO" : ["robot_torso"],
+		"OBJECT_TYPE_ROBOT_LEFT_HAND" : ["robot_left_hand"],
+		"OBJECT_TYPE_ROBOT_RIGHT_HAND" : ["robot_right_hand"],
+		"OBJECT_TYPE_U_BLOCK" : ["u_block", "left u", "right u"]
 		}
 
-		self.object_color = {
+		object_color = {
 		"OBJECT_COLOR_RED" : "red",
 		"OBJECT_COLOR_BLUE" : "blue",
 		"OBJECT_COLOR_GREEN" : "green",
@@ -33,9 +35,8 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
                 # start lcm
                 self.lc = lcm.LCM()
                 # subscribe to world state channel
-                self.subscription = self.lc.subscribe( "STATE_MODEL_ROCBOT", self.world_state_handler )
+                self.subscription = self.lc.subscribe( "STATE_MODEL_ROCBOT", RocbotSensorHandler.world_state_handler )
                 self.state_message = dict()
-
 
 # state message format
 #  int64_t timestamp;
@@ -45,31 +46,69 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 #  int32_t num_state_bodies;
 #  state_body_msg_t state_bodies[ num_state_bodies ];
 
-        def world_state_handler( self, channel, data ):
+	@staticmethod
+        def world_state_handler( channel, data ):
 		"""
 		Handler for rocbot LCM state messages
 
 		channel (string): lcm channel name
 		data (dict): data message read on the channel
 		"""
-                msg = state_model_msg_t.decode(data)
-		return msg
+		global state_msg
+                state_msg = state_model_msg_t.decode(data)
 
 	def listen_for_state(self):
-		try:
-			# clear old data
-			self.state_message = dict()
-			while True:
-				self.lc.handle()
-				# stop when message keys start repeating i.e. we have all objects
-				if msg.id in self.state_message:
-					break
-				self.state_message[msg.id] = msg
-			print '#### listened and got', self.state_message.keys()
-				
-                except KeyboardInterrupt:
-                        pass
+		# clear old data
+		self.state_message = dict()
+		none_counter = 0
+		while True:
+			self.lc.handle()
+			# stop when message keys start repeating i.e. we have all objects
+			if state_msg.id in self.state_message:
+				break
+			self.state_message[state_msg.id] = state_msg
 
+	@staticmethod
+	def id_to_type( msg_id ):
+		"""
+		Looks up an object symbol type by a name, returns the type if found or None otherwise
+
+		msg_id (string): id string as returned in the lcm message type
+		"""
+		for obj_type in RocbotSensorHandler.object_types:
+			print msg_id, obj_type
+			if msg_id in RocbotSensorHandler.object_types[ obj_type ]:
+				return obj_type
+		return None
+
+	@staticmethod
+	def test_object_color( obj, color="na" ):
+		"""
+		Check that the object message obj matches the given color
+
+		obj (dict): message with object
+		color (string): color
+		"""
+		if color=="na":
+			return True
+		#TODO
+		return True
+	
+	def matching_objects(self, object_type, object_color="na"):
+		"""
+		Return a list of all objects in the message list
+		that match a type and color
+
+		object_type (string): ltl_h2sl object type
+		object_color (string): ltl_h2sl object color
+		"""
+		result = list()
+		for msg_id in self.state_message:
+			if object_type == RocbotSensorHandler.id_to_type( msg_id ):
+				print '###'
+				if RocbotSensorHandler.test_object_color( self.state_message[ msg_id ], object_color ):
+					result.append( self.state_message[ msg_id ] )
+		return result	
 	# TODO
 	def sensor_type_observed(self, object_type, object_color, initial=False):
 		"""
@@ -80,8 +119,6 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 			return False
 		try:
 			listen_for_state()
-			print '#', self.RocbotInitHandler.keys()
-			return True
 		except:
 			pass
 			
@@ -97,7 +134,6 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 			return False
 		try:
 			listen_for_state()
-			print self.RocbotInitHandler.keys()
 		except:
 			pass
 			
@@ -113,7 +149,6 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 			return False
 		try:
 			listen_for_state()
-			print self.RocbotInitHandler.keys()
 		except:
 			pass
 			
@@ -129,7 +164,6 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 			return False
 		try:
 			listen_for_state()
-			print self.RocbotInitHandler.keys()
 		except:
 			pass
 			
@@ -149,8 +183,6 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 			return False
 		try:
 			listen_for_state()
-			print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-			print self.RocbotInitHandler.keys()
 		except:
 			pass
 			
@@ -169,9 +201,19 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 		if initial:
 			return False
 		try:
-			listen_for_state()
-			print '###################'
-			print self.RocbotInitHandler.keys()
+			self.listen_for_state()
+			# find all objects that match the type and color 
+			obj1 = self.matching_objects( object_type1, object_color1 )
+			obj2 = self.matching_objects( object_type2, object_color2 )
+
+			# for all possible pairs, test if the relation holds 
+			# return at the first positive
+
+			for o1 in obj1:
+				for o2 in obj2:
+					# TODO test spatial relation, not just finding objects
+					return True
+			
 		except:
 			pass
 			
@@ -191,7 +233,6 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 			return False
 		try:
 			listen_for_state()
-			print self.RocbotInitHandler.keys()
 		except:
 			pass
 			
@@ -211,7 +252,6 @@ class RocbotSensorHandler(handlerTemplates.SensorHandler):
 			return False
 		try:
 			listen_for_state()
-			print self.RocbotInitHandler.keys()
 		except:
 			pass
 			
